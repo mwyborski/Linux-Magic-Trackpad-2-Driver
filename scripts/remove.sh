71d9c4a287a5ad575d1771d6338ade6757178c00
Adam Heath <adam@polleverywhere.com>
2018-06-05 09:57:55 +0800

    First stab at dkms config

---
+++ scripts/remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+dkms_name="hid-magicmouse-dkms"
+dkms_version="4.17+magictrackpad2"
+
+if dkms status -m $dkms_name -v $dkms_version | egrep '(added|built|installed)' >/dev/null ; then
+  # if dkms bindings exist, remove them
+  dkms remove $dkms_name/$dkms_version --all
+fi
