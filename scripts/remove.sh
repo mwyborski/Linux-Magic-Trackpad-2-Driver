c1ede71fb7fb2dbccd7dea008e2cba4f1e92c046
robotrovsky <robotrovsky@users.noreply.github.com>
2018-07-25 10:05:23 +0200

    Update script to 4.18

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -3,7 +3,7 @@
 set -e
 
 dkms_name="hid-magicmouse-dkms"
-dkms_version="4.17+magictrackpad2"
+dkms_version="4.18+magictrackpad2"
 
 if dkms status -m $dkms_name -v $dkms_version | egrep '(added|built|installed)' >/dev/null ; then
   # if dkms bindings exist, remove them
